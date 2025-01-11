4c8f2213cc5b17b0a3ce668f625c11a55a85e5e7
Nhân Trần Thanh <nhancmvn12@gmail.com>
2025-01-12 01:00:33 +0700

    slide home

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('slideContainer') slideContainer!: ElementRef<HTMLDivElement>;
+
+  constructor(private router: Router) {}
+
+  commonInfo = {
+    name: 'Trần Thanh Nhân',
+    description: 'Software Engineer',
+  };
+  slides = [
+    '/assets/images/slideshows/1.jpeg',
+    '/assets/images/slideshows/2.jpeg',
+    '/assets/images/slideshows/3.jpeg',
+    '/assets/images/slideshows/4.jpeg',
+    '/assets/images/slideshows/5.jpeg',
+  ];
+
+  ngAfterViewInit(): void {
+  }
+
+  navigateToStore(): void {
+    this.router.navigate(['/home/store']);
+  }
+  moveNext(): void {
+    const slides = this.slideContainer.nativeElement.querySelectorAll('.item');
+    if (slides.length) {
+      this.slideContainer.nativeElement.appendChild(slides[0]);
+    }
+  }
+
+  movePrev(): void {
+    const slides = this.slideContainer.nativeElement.querySelectorAll('.item');
+    if (slides.length) {
+      this.slideContainer.nativeElement.prepend(slides[slides.length - 1]);
+    }
+  }
 
 }
