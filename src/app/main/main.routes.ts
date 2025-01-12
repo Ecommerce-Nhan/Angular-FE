fa8e2c782e28c8590e185bbc93b810b5ab3639cf
Nhân Trần Thanh <nhancmvn12@gmail.com>
2025-01-12 19:07:32 +0700

    Fix color

--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const mainRoutes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+    { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
     { path: 'store', loadComponent: () => import('./store/store.component').then(m => m.StoreComponent) }
   ];
